454c7aab8a703f17ef63f9728321d0efa252d5ff
Ivan Herreros <ivanherreros@gmail.com>
2024-01-02 13:50:32 +0100

    fix package structure

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(name='embedding-cvx-projection',
       version='0.0.1',
       description='Embedding projection using CVXPY',
       author='Ivan Herreros',
       author_email='ivan.herreros@gmail.com',
-      packages=find_packages(where='src'),
-      package_dir={'': 'src'},
+      package_dir={'embedding_cvx_projection': 'src'},
       install_requires=[
           'cvxpy'
         ])
